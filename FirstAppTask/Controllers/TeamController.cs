974c05fcd940037a1f830596262d799838685f2e
anaramanli <riadhamidov@gmail.com>
2024-05-06 19:19:59 +0400

    first commit

---
+++ FirstAppTask/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAppTask.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstAppTask/Program.cs
@@ -0,0 +1,20 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace FirstAppTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+            app.MapControllerRoute("default" , "{controller=home}/{action=index}");
+
+            app.Run();
+
+        }
+    }
+}
